06bdffff8a0c0af9a732fb5be087225726990f71
Bruno Faria <nunes@outlook.com>
2019-02-18 21:29:19 +1100

    fixed demo

--- Demo.sh
+++ Demo.sh
@@ -1,131 +1,93 @@
+# This is based on MSFT provided samples. 
+# Detailed information can be found below
+# https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-prepare-app
 # Pre-requisites
-# docker ; container runtime
+# docker engine ; container runtime
+# https://www.docker.com/get-started
 # az ; azure cli  
+# https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
 # az aks install-cli ; kubernetes cli
 
-# Part 1 - Prepare App for AKS
-
-# Git clone sample app
+# Git clone sample app for our demo
 # Sample app contains the base image for nginx+flask, the frontend app and a redis instance
 git clone https://github.com/Azure-Samples/azure-voting-app-redis.git
 
-# Cd to Dir & Build image using Docker-Compose
-# docker-compose up: Builds, (re)creates, starts, and attaches to containers for a service.
+# use docker-compose to build, creates, starts the container locally.
 # -d is detach mode: runs in the background
 cd azure-voting-app-redis
-docker-compose up
+docker-compose up -d
 
-# list 
+# check if image is available
 docker images
 
-# list running processes
+# check if container is running
 docker ps
 
-# test app
+# Test the app by browsing endpoint
 http://localhost:8080
 
-# Clean up resources
-docker-compose down
-
-# Part 2 - Azure Container Registry
-# Create RG
+# To deploy the container to AKS, first lets create a resource group for our demo env
 az group create --name "aksdemo-rg" --location australiaeast
 
-# Create ACR
-az acr create --resource-group "aksdemo-rg" --name aksdemoacr01 --sku Basic
+# And an Azure Container Registry for storing our container images in a private repository
+az acr create --resource-group "aksdemo-rg" --name aksdemoacr --sku Basic
 
-# Login to ACR - Individual Authentication
-az acr login --name aksdemoacr01
+# After deployment, login to ACR to be able to run query
+az acr login --name aksdemoacr
 
 # To use azure-vote-front image with ACR, the image needs to be tagged with the login server address of the registry. 
 # Get the login server address
 az acr list --resource-group aksdemo-rg --query "[].{acrLoginServer:loginServer}" --output table
 
-# Tag image with ACR Login Server and add version
+# Tag image with ACR Login Server and add a release version. Im calling it v1
 # https://docs.docker.com/engine/reference/commandline/tag/
-docker tag azure-vote-front aksdemoacr01.azurecr.io/azure-vote-front:v1
+docker tag azure-vote-front aksdemoacr.azurecr.io/azure-vote-front:v1
 
-# Push image to registry
-docker push aksdemoacr01.azurecr.io/azure-vote-front:v1
+# Push image to ACR
+docker push aksdemoacr.azurecr.io/azure-vote-front:v1
 
-# See tags for specific image
-az acr repository show-tags --name aksdemoacr01 --repository azure-vote-front --output table
+# Check container repository for all available Tags
+az acr repository show-tags --name aksdemoacr --repository azure-vote-front --output table
 
-# Part 3 - Create Kubernetes Cluster
-
-# Create a service principal
+# Before deploying AKS create the Service Principal that your cluster will use
 # https://docs.microsoft.com/en-au/azure/aks/kubernetes-service-principal
 az ad sp create-for-rbac --skip-assignment
 
-# Configure ACR authentication
-az acr show --resource-group aksdemo-rg --name aksdemoacr01 --query "id" --output tsv
+# Also, make sure AKS can pull images from ACR by giving the SP permission to pull images
+# To create a role, use the service princiapal (app) id and the id of the ACR 
+az acr show --resource-group aksdemo-rg --name aksdemoacr --query "id" --output tsv
+az role assignment create --assignee <app_ID> --scope <acr_ID> --role acrpull
 
-# Grand access for AKS cluster to pull images from ACR
-az role assignment create --assignee 4574df10-2fe7-48e0-82d5-a4b1b3330875 --scope /subscriptions/cc0ff371-2090-42de-84fc-c0013f955131/resourceGroups/aksdemo-rg/providers/Microsoft.ContainerRegistry/registries/aksdemoacr01 --role acrpull
-
-# Create the Kubernetes Cluster
-az aks create --resource-group aksdemo-rg --name aksdemoaks001 --node-count 1 --service-principal 4574df10-2fe7-48e0-82d5-a4b1b3330875 --client-secret f104201a-cc21-45d4-8c7f-17b6dc5c956c --generate-ssh-keys
+# Finally deploy the AKS Cluster
+az aks create --resource-group aksdemo-rg --name aksdemoaks --node-count 1 --service-principal <app_ID> --client-secret <app_Secret> --generate-ssh-keys
 
 # Connect to cluster
-az aks get-credentials --resource-group aksdemo-rg --name aksdemoaks001
+az aks get-credentials --resource-group aksdemo-rg --name aksdemoaks
 
-# Get nodes
+# Check for nodes
 kubectl get nodes
 
-# Part 4 - Deploy App to AKS
-
-# Get ACR Login Server and update manifest
-az acr list --resource-group aksdemo-rg --query "[].{acrLoginServer:loginServer}" --output table
-
-# Parses and creates the Kubernetes object
+# Finally deploy your container to AKS
+# Edit the file azure-vote-all-in-one-redis.yaml and update the azure-vote-front image to point to the ACR endpoint (line 47)
+# image: aksdemoacr.azurecr.io/azure-vote-front:v1
+# Create the resource in AKS
 kubectl apply -f azure-vote-all-in-one-redis.yaml
 
-# Monitor progress
+# You can use the cmd below to monitor the progress
 kubectl get service azure-vote-front --watch
 
-# Part 5 - Scaling your Applications
-
-# Get Pods
+# Check for running pods
 kubectl get pods
 
-# Manually Scale Pod
+# To manually scale a pod, use the cmd below. This will scale up to 3 replicas
 kubectl scale --replicas=3 deployment/azure-vote-front
 
-# Auto scale pods
-# Pre-requisites Metrics server - comes with Kubernetes 1.10+
-# az aks show --resource-group aksdemo-rg --name aksdemoaks001 --query kubernetesVersion
-
-# Autoscale pods based on cpu utilisation
+# You can also autoscale pods based on cpu utilisation
 kubectl autoscale deployment azure-vote-front --cpu-percent=50 --min=3 --max=10
 
-# Manually scale AKS Nodes
-az aks scale --resource-group aksdemo-rg --name aksdemoaks001 --node-count 3
-
-# Part 6 - Updating your Applications
-
-# Make changes to app and update the container image
-docker-compose up --build -d
-
-# Tag your image as v2
-docker tag azure-vote-front aksdemoacr01.azurecr.io/azure-vote-front:v2
-
-# Push to ACR
-docker push aksdemoacr01.azurecr.io/azure-vote-front:v2
-
-# Deploy the updated application
-kubectl set image deployment azure-vote-front azure-vote-front=aksdemoacr01.azurecr.io/azure-vote-front:v2
-
-# Test the application
-kubectl get service azure-vote-front
-
-# Part 7 - Upgrade Kubernetes
-az aks get-upgrades --resource-group aksdemo-rg --name aksdemoaks001 --output table
-
-# Upgrade the cluster
-az aks upgrade --resource-group aksdemo-rg --name aksdemoaks001 --kubernetes-version 1.12.5
-
-# Validate the upgrade
-az aks show --resource-group aksdemo-rg --name aksdemoaks001 --output table
+# We can also manually scale the nodes 
+# autoscale is in preview, but available if using AKS 1.12.5+
+az aks scale --resource-group aksdemo-rg --name aksdemoaks --node-count 3
 
 
  
